c7b33af14c5204f27b6184cb502446f9e5be7636
aabda2000 <38082725+aabda2000@users.noreply.github.com>
2022-03-09 14:20:53 +0100

    update config.php

--- config.php
+++ config.php
@@ -1,16 +1,12 @@
 <?php 
-	session_start();
+	session_start();// start user session
 	// connect to database
 	$conn = mysqli_connect("localhost", "root", "orange", "blog");
-  
-
 	if (!$conn) {
 
 		die("Error connecting to database: " . mysqli_connect_error());
 	}
-    
-
-    // define global constants
+    // define global constants : you might change those globals
 	define ('ROOT_PATH', realpath(dirname(__FILE__)));
 	define('BASE_URL', 'http://localhost:8000/');
 ?>
