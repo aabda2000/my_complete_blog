ba187e893a6a146baefc17dc29a0aa1d75bd1560
insa cvl <insa@asterix.insa.fr>
2022-03-09 13:56:58 +0100

    initial commit

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php 
+	session_start();
+	// connect to database
+	$conn = mysqli_connect("localhost", "root", "orange", "blog");
+  
+
+	if (!$conn) {
+
+		die("Error connecting to database: " . mysqli_connect_error());
+	}
+    
+
+    // define global constants
+	define ('ROOT_PATH', realpath(dirname(__FILE__)));
+	define('BASE_URL', 'http://localhost:8000/');
+?>
